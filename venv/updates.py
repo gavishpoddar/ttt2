680136c43f171072759303ac97f97fbff5f7d0ee
gavish <gavishpoddar@hotmail.com>
2020-02-06 02:22:21 +0530

    Commit

--- venv/updates.py
+++ venv/updates.py
@@ -8,8 +8,11 @@ from dataBuffer import quote
 def updates(symbol, moneyControl_Symbol):
  threads = []
 
- min5 = 0
  min1 = 1
+ min5 = 0
+ min30 = 0
+
+
 
 
  if min1 == 1 :
@@ -19,11 +22,17 @@ def updates(symbol, moneyControl_Symbol):
    threads.append(ltp_1m)
 
  if datetime.utcnow().minute % 5 == 0 and min5 == 1:
-
    macd_5m = threading.Thread(target=data_5m.getMACD, args=(symbol,))
    ltp_5m = threading.Thread(target=quote, args=(moneyControl_Symbol,))
    threads.append(macd_5m)
    threads.append(ltp_5m)
+
+ if datetime.utcnow().minute % 30 == 0 and min30 == 1:
+
+   macd_30m = threading.Thread(target=data_5m.getMACD, args=(symbol,))
+   ltp_30m = threading.Thread(target=quote, args=(moneyControl_Symbol,))
+   threads.append(macd_30m)
+   threads.append(ltp_30m)
 
  for x in threads:
      x.start()
